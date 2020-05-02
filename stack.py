00515fad1250dc0d556f3ad9df782a7e070d3c17
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-05-01 17:06:16 -0700

    Array Stack time complexity

--- stack.py
+++ stack.py
@@ -80,7 +80,7 @@ class ArrayStack(object):
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1), first list item"""
         self.list.insert(0, item)
 
     def peek(self):
@@ -93,7 +93,7 @@ class ArrayStack(object):
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1), first list item"""
         # TODO: Remove and return top item, if any
         if self.is_empty():
             raise ValueError('Stack empty')
